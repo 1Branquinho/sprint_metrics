43f8990b0d4770db6e279e3c34654affd3d6246b
Eduardo de Alarcão Branquinho <branquinhodev@gmail.com>
2026-02-10 13:38:47 -0300

    commit for post and get sprints

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,15 @@ from fastapi import FastAPI
 from app.db.session import engine
 from app.models.base import Base
 import app.models
+from app.api.sprints import router as sprints_router
 
 app = FastAPI(title="Sprint Metrics MVP")
 
 @app.on_event("startup")
 def on_startup():
     Base.metadata.create_all(bind=engine)
+
+app.include_router(sprints_router)
+
 
 #uvicorn app.main:app --reload --port 8000
